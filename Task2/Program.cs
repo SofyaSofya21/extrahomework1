5dcd22d64165969559687daa8d2470d21406f9fc
Sofya <sofyaguseva1@gmail.comgit>
2022-11-13 03:19:22 +0100

    Task 2 created

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// 2. Есть программа с бесконечным циклом. 
+// Когда пользователь вводит exit программа заканчивается
+
+string signal = "0";
+string correctSignal = "exit";
+
+Console.WriteLine("Что говорит маленькая собачка, когда видит большую собаку?");
+
+while(signal != correctSignal)
+{
+    Console.WriteLine("ГАВ! ГАВ! ГАВ! ГАВ! ГАВ! ГАВ! ГАВ! ГАВ!");
+    signal = Console.ReadLine();
+}
